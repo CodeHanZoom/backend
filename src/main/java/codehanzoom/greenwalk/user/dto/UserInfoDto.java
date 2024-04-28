0234f1149d50e7981dfa5009fd0ff21ccc432456
turtle <lsiwh32749@gmail.com>
2024-04-28 16:11:54 +0900

    User 정보 반환하는 Controller 추가, User 엔티티에  totalStep, totalTrashCount, totalWalkingDistance 필드 추가

---
+++ src/main/java/codehanzoom/greenwalk/user/dto/UserInfoDto.java
@@ -0,0 +1,24 @@
+package codehanzoom.greenwalk.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserInfoDto {
+
+    public Long id;
+
+    public String name;
+
+    public int totalPoint;
+
+    public int totalDonation;
+
+    public int totalStep;
+
+    private int totalTrashCount;
+
+    private float totalWalkingDistance;
+
+}
